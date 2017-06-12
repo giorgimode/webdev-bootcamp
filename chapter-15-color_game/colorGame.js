39a0a6266e26fa9afb894d3aeabf53e2554744f1
giorgimode <modegiorgi@gmail.com>
2017-06-12 14:52:57 +0200

    color game: add fixed color and listener

--- chapter-15-color_game/colorGame.js
+++ chapter-15-color_game/colorGame.js
@@ -1,14 +1,30 @@
 var colors = [
     "rgb(255, 0, 0)",
-    "rgb(255, 255, 0",
-    "rgb(0, 255, 0",
-    "rgb(0, 255, 255",
-    "rgb(0, 0, 255",
-    "rgb(255, 0, 255"
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
 ];
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
 
-for(var i = 0; i< squares.length; i++) {
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+    // add initial colors
     squares[i].style.background = colors[i];
+
+    //add listeners to squares
+    squares[i].addEventListener("click", function () {
+        var clickedColor = this.style.background;
+
+        if (clickedColor === pickedColor) {
+            alert("Correct!");
+        } else {
+            alert("-Wrong!");
+        }
+    });
 }
