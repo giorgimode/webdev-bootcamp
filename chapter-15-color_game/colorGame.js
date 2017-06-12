aa05e8296e350db928433cfae1f74ec7c6e166f5
giorgimode <modegiorgi@gmail.com>
2017-06-12 16:20:15 +0200

    color game: added easy & hard modes

--- chapter-15-color_game/colorGame.js
+++ chapter-15-color_game/colorGame.js
@@ -1,4 +1,4 @@
-var colors = generateRandomColors(6);
+var colors = generateRandomColors(numberOfSquares);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickRandomColor();
@@ -6,9 +6,12 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+var numberOfSquares = 6;
 
 resetButton.addEventListener("click", function () {
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numberOfSquares);
     pickedColor = pickRandomColor();
     colorDisplay.textContent = pickedColor;
     for (var i = 0; i < squares.length; i++) {
@@ -17,6 +20,38 @@ resetButton.addEventListener("click", function () {
     h1.style.background = "#232323";
     messageDisplay.textContent = "";
 });
+
+easyBtn.addEventListener("click", function () {
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numberOfSquares = 3;
+    colors = generateRandomColors(numberOfSquares);
+    pickedColor = pickRandomColor();
+    colorDisplay.textContent = pickedColor;
+
+    for (var i = 0; i < squares.length; i++) {
+        if (colors[i]){
+            squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardBtn.addEventListener("click", function () {
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    numberOfSquares = 6;
+    colors = generateRandomColors(numberOfSquares);
+    pickedColor = pickRandomColor();
+    colorDisplay.textContent = pickedColor;
+
+    for (var i = 0; i < squares.length; i++) {
+            squares[i].style.background = colors[i];
+            squares[i].style.display = "block";
+    }
+});
+
 
 colorDisplay.textContent = pickedColor;
 
