6b0b26f84cdec216f16f79598df6c7c24233cad2
giorgimode <modegiorgi@gmail.com>
2017-06-12 15:38:03 +0200

    color game: generating random colors

--- chapter-15-color_game/colorGame.js
+++ chapter-15-color_game/colorGame.js
@@ -1,16 +1,10 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickRandomColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -25,6 +19,7 @@ for (var i = 0; i < squares.length; i++) {
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct";
             changeColor(clickedColor);
+            h1.style.background = pickedColor;
         } else {
             this.style.background = "#232323";
             messageDisplay.textContent = "Try Again";
@@ -41,4 +36,22 @@ function changeColor(color) {
 function pickRandomColor() {
     var randIndex = Math.floor(Math.random() * colors.length);
     return colors[randIndex];
+}
+
+function generateRandomColors(number) {
+    var colors = [];
+
+    for (var i = 0; i < number; i++) {
+        colors.push(generateRandomColor());
+    }
+
+    return colors;
+}
+
+function generateRandomColor() {
+    var colorIndexR = Math.floor(Math.random() * 256);
+    var colorIndexG = Math.floor(Math.random() * 256);
+    var colorIndexB = Math.floor(Math.random() * 256);
+
+    return "rgb(" + colorIndexR + ", " + colorIndexG + ", " + colorIndexB + ")";
 }
